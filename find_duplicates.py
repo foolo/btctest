65d3ca8a3e0708fde27daaf0ad1b795b47b671b2
Olof Andersson <olof.andersson@gmail.com>
2025-01-30 09:59:24 +0100

    M find_duplicates.py

--- find_duplicates.py
+++ find_duplicates.py
@@ -9,7 +9,8 @@ def find_duplicates():
 
 	line_count = 0
 	with open(r_archive_file_sorted, 'r') as archive_file:
-		previous_r_value_hex = ""
+		current_r_value_hex = ""
+		r_value_stack: list[str] = []
 		for line in archive_file:
 			parts = line.strip().split('\t')
 			if len(parts) != 4:
@@ -18,9 +19,13 @@ def find_duplicates():
 			block_id = int(parts[1])
 			tx_index = int(parts[2])
 			input_index = int(parts[3])
-			if r_value_hex == previous_r_value_hex:
-				print(f'duplicate r_value_hex: {r_value_hex}, block_id: {block_id}, tx_index: {tx_index}, input_index: {input_index}')
-			previous_r_value_hex = r_value_hex
+			if r_value_hex != current_r_value_hex:
+				if len(r_value_stack) > 1:
+					for r_value in r_value_stack:
+						print(r_value)
+				r_value_stack.clear()
+			r_value_stack.append(f'duplicate r_value_hex: {r_value_hex}, block_id: {block_id}, tx_index: {tx_index}, input_index: {input_index}')
+			current_r_value_hex = r_value_hex
 			line_count += 1
 	print(f'parsed {line_count} lines')
 
