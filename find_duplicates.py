84e6203ba9ada28daf760ad9efc0b11674634415
Olof Andersson <olof.andersson@gmail.com>
2025-01-30 10:26:28 +0100

    M find_duplicates.py

--- find_duplicates.py
+++ find_duplicates.py
@@ -12,19 +12,17 @@ def find_duplicates():
 		current_r_value_hex = ""
 		r_value_stack: list[str] = []
 		for line in archive_file:
-			parts = line.strip().split('\t')
+			line = line.strip()
+			parts = line.split('\t')
 			if len(parts) != 4:
 				raise ValueError(f'len(parts) != 4: {parts}')
 			r_value_hex = parts[0]
-			block_id = int(parts[1])
-			tx_index = int(parts[2])
-			input_index = int(parts[3])
 			if r_value_hex != current_r_value_hex:
 				if len(r_value_stack) > 1:
 					for r_value in r_value_stack:
 						print(r_value)
 				r_value_stack.clear()
-			r_value_stack.append(f'duplicate r_value_hex: {r_value_hex}, block_id: {block_id}, tx_index: {tx_index}, input_index: {input_index}')
+			r_value_stack.append(line)
 			current_r_value_hex = r_value_hex
 			line_count += 1
 	print(f'parsed {line_count} lines')
